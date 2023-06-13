dc658e176fae74f03a14a69c700e7ec62b3b2a3e
Ioan Anghel <ioan.anghel123@gmail.com>
2023-06-14 02:09:56 +0300

    Added sorting functions and Reset button values

--- resurse/js/produse.js
+++ resurse/js/produse.js
@@ -141,4 +141,79 @@ window.onload=function() {
             }
         }
     }
+
+    document.getElementById("resetare").onclick= function(){
+                
+        document.getElementById("inp-nume").value="";
+        
+        document.getElementById("inp-pret").value=document.getElementById("inp-pret").max;
+        document.getElementById("inp-categorie").value="toate";
+        document.getElementById("i_rad4").checked=true;
+        let team_list = document.getElementsByClassName("team_check");
+        for(let team of team_list){
+            team.checked=true;
+        }
+        let driver_list = document.getElementsByClassName("driver_check");
+        for(let driver of driver_list){
+            driver.checked=true;
+        }
+        var produse=document.getElementsByClassName("produs");
+        document.getElementById("infoRange").innerHTML="(0)";
+        for (let prod of produse){
+            prod.style.display="block";
+        }
+    }
+
+    function sortare (semn){
+        var produse=document.getElementsByClassName("produs");
+        var v_produse= Array.from(produse);
+        v_produse.sort(function (a,b){
+            let pret_a=parseFloat(a.getElementsByClassName("val-pret")[0].innerHTML);
+            let pret_b=parseFloat(b.getElementsByClassName("val-pret")[0].innerHTML);
+            if(pret_a==pret_b){
+                let nume_a=a.getElementsByClassName("val-nume")[0].innerHTML;
+                let nume_b=b.getElementsByClassName("val-nume")[0].innerHTML;
+                return semn*nume_a.localeCompare(nume_b);
+            }
+            return semn*(pret_a-pret_b);
+        });
+        for(let prod of v_produse){
+            prod.parentElement.appendChild(prod);
+        }
+    }
+    document.getElementById("sortCrescNume").onclick=function(){
+        sortare(1);
+    }
+    document.getElementById("sortDescrescNume").onclick=function(){
+        sortare(-1);
+    }
+
+    window.onkeydown= function(e){
+        if (e.key=="c" && e.altKey){
+            if(document.getElementById("info-suma"))
+                return;
+            var produse=document.getElementsByClassName("produs");
+            let suma=0;
+            for (let prod of produse){
+                if(prod.style.display!="none")
+                {
+                    let pret=parseFloat(prod.getElementsByClassName("val-pret")[0].innerHTML);
+                    suma+=pret;
+                }
+            }
+            
+            let p=document.createElement("p");
+            p.innerHTML=suma;
+            p.id="info-suma";
+            ps=document.getElementById("p-suma");
+            container = ps.parentNode;
+            frate=ps.nextElementSibling
+            container.insertBefore(p, frate);
+            setTimeout(function(){
+                let info=document.getElementById("info-suma");
+                if(info)
+                    info.remove();
+            }, 1000)
+        }
+    }
 }
