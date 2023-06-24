33238ef22a881d5b9ef0b4791b1ebe22d4326699
Ioan Anghel <ioan.anghel123@gmail.com>
2023-06-24 14:54:15 +0300

    Added winter theme -> available options: light, dark, winter

--- resurse/js/tema.js
+++ resurse/js/tema.js
@@ -1,11 +1,18 @@
 let tema=localStorage.getItem("tema");
 if(tema)
-    document.body.classList.add("dark");
+    document.body.classList.add(tema);
         
 window.addEventListener("DOMContentLoaded", function(){
     document.getElementById("tema").onclick= function(){
         if(document.body.classList.contains("dark")){
-            document.body.classList.remove("dark")
+            document.body.classList.remove("dark");
+            localStorage.removeItem("tema");
+            document.body.classList.add("winter");
+            localStorage.setItem("tema","winter");
+            tema=localStorage.getItem("tema");
+        }
+        else if(document.body.classList.contains("winter")){
+            document.body.classList.remove("winter")
             localStorage.removeItem("tema");
         }
         else{
